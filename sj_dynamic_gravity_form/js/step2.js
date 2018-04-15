ae3f38adbcdf586a0e41145ea13cac8159c4fa2a
STeven <sanguine.steve@gmail.com>
2018-04-15 20:34:00 +0200

    New steps UI functionality set up

--- sj_dynamic_gravity_form/js/step2.js
+++ sj_dynamic_gravity_form/js/step2.js
@@ -135,7 +135,8 @@ function step3(){
         jQuery('[rider-details]').hide();
         jQuery('#field_'+gf_form_id+'_54 .gsection_title').text('Step 3 - Insurance Details');
         jQuery('#field_'+gf_form_id+'_40')
-        .after(sj_rider_insurance_wrapper.clone(true))
+        .after(sj_rider_insurance_wrapper.clone(true)
+        .append('<a href="#gform_'+gf_form_id+'" class="button sj_next" style="float:right;" onclick="step4()">Next Step</a>'))
         .show(); 
         // .attr('rider-details', riderObjs[key]['riderNumber'])
         // .prepend('<h2 class="sj-rider-details-heading">Rider '+ riderObjs[key]['riderNumber'] + ' Passenger Details</h2>')
@@ -144,9 +145,32 @@ function step3(){
         // .attr('passenger', 'true')
                 
 
-    console.log('step3');
+    // console.log('step3');
     jQuery('#field_'+gf_form_id+'_40').hide();
 }
+
+function step4(){
+    jQuery('.sj_rider_insurance_wrapper ').hide();
+    jQuery('#field_'+gf_form_id+'_54 .gsection_title').text('Step 4 - Check and Confirm your booking');
+    jQuery('#field_'+gf_form_id+'_40')
+        .append('Placeholder')
+        .show();
+    console.log('step4');
+        // jQuery('[rider-details]').hide();
+        // jQuery('#field_'+gf_form_id+'_54 .gsection_title').text('Step 3 - Insurance Details');
+        // jQuery('#field_'+gf_form_id+'_40')
+        // .after(sj_rider_insurance_wrapper.clone(true)
+        // .append('<a href="#gform_'+gf_form_id+'" class="button sj_next" style="float:right;" onclick="nextRiderDetails()">Next Rider</a>'))
+        // .show(); 
+        // .attr('rider-details', riderObjs[key]['riderNumber'])
+        // .prepend('<h2 class="sj-rider-details-heading">Rider '+ riderObjs[key]['riderNumber'] + ' Passenger Details</h2>')
+        // .append('<a href="#gform_'+gf_form_id+'" class="button sj_back" onclick="prevRiderDetails()">Back</a>')
+        // .append('<a href="#gform_'+gf_form_id+'" class="button sj_next" style="float:right;" onclick="nextRiderDetails()">Next Rider</a>'))
+        // .attr('passenger', 'true')
+                
+    jQuery('#field_'+gf_form_id+'_40').hide();
+}
+
 
 
 function prevRiderDetails(){
