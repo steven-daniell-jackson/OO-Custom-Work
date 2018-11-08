9cc4d76381bb367740786197214d9c1725d293b3
STeven <sanguine.steve@gmail.com>
2018-11-08 19:30:06 +0200

    Resolved issue with product not created hashed download id in Woocommerce

--- sj_custom_woocommerce_add_product/sj_custom_woocommerce_add_product.php
+++ sj_custom_woocommerce_add_product/sj_custom_woocommerce_add_product.php
@@ -7,14 +7,14 @@ Author Contact: stevenjackson.sanguine@gmail.com
 Date Created: 10 July 2018
 */
 
-// function register_custom_menu_page() {
-//	add_menu_page('Add product', 'Add product', 'add_users', 'addproduct', '_sj_custom_menu', null, 6);
-// }
-// add_action('admin_menu', 'register_custom_menu_page');
-// add_filter('wp_dashboard_setup', 'register_custom_menu_page');
-// function _sj_custom_menu(){
-//	include( plugin_dir_path( __FILE__ ) ."/template/add-product.php");
-// }
+function register_custom_menu_page() {
+	add_menu_page('Add product', 'Add product', 'add_users', 'addproduct', '_sj_custom_menu', null, 6);
+}
+add_action('admin_menu', 'register_custom_menu_page');
+add_filter('wp_dashboard_setup', 'register_custom_menu_page');
+function _sj_custom_menu(){
+	include( plugin_dir_path( __FILE__ ) ."/template/add-product.php");
+}
 
 // add_filter('upload_mimes', 'restrict_mime');
 function restrict_mime($mimes) {
@@ -24,13 +24,6 @@ function restrict_mime($mimes) {
 	return $mimes;
 }
 
-// function sjCustomWoocommerceAddProductCSS(){
-//	$src =  plugins_url( ) ."/sj_custom_woocommerce_add_product/css/sj_custom_woocommerce_add_product.css";
-//	$handle = "sjCustomWoocommerceAddProductCss";
-//	wp_register_script($handle, $src);
-//	wp_enqueue_style($handle, $src, array(), false, false);
-// }
-// add_action('admin_head', 'sjCustomWoocommerceAddProductCSS', 30);
 
 add_action('admin_enqueue_scripts', 'sjCustomWoocommerceAddProductCSS');
 function sjCustomWoocommerceAddProductCSS() {
@@ -54,8 +47,8 @@ function sj_uploadscript() {
 add_action('admin_enqueue_scripts', 'myplugin_ajaxurl');
 function myplugin_ajaxurl() {
 	echo '<script type="text/javascript">
-   	var ajaxurl = "' . admin_url('admin-ajax.php') . '";
-   	</script>';
+	var ajaxurl = "' . admin_url('admin-ajax.php') . '";
+	</script>';
 }
 
 // add_filter('wp_handle_upload_prefilter', 'upload_image_validation');
@@ -175,21 +168,19 @@ function so_wp_ajax_function() {
 
 	// print_r ($sj_download_full[0]);
 
-	$small[] = array(
+	$small[md5( $sj_download_small) ] = array(
 		'name' => $sj_product_name . '-small',
 		'file' => $sj_download_small[0]
 	);
-	$medium[] = array(
+	$medium[md5( $sj_download_medium) ] = array(
 		'name' => $sj_product_name . '-medium',
 		'file' => $sj_download_medium[0]
 	);
-	$full[] = array(
+	$full[md5( $sj_download_full) ] = array(
 		'name' => $sj_product_name . '-full',
 		'file' => $sj_download_full[0]
 	);
 
-	// print_r(wp_get_attachment_image_src( $sj_product_image_id, 'download-small'));
-	// print_r($small);
 
 	$image_sizes = wp_get_attachment_metadata($sj_product_image_id);
 
@@ -197,59 +188,73 @@ function so_wp_ajax_function() {
 	// print_r($image_sizes['sizes']['full']);
 	// insert variations post_type
 
-	$i = 1;
-	while ($i <= 3) {
-		$my_post = array(
-			'post_title' => 'Variation #' . $i . ' of ' . esc_attr(strip_tags($_POST['productName'])) ,
-			'post_name' => 'product-' . $post_id . '-variation-' . $i,
-			'post_status' => 'publish',
-			'post_parent' => $post_id,
-			'post_type' => 'product_variation',
-			'guid' => home_url() . '/?product_variation=product-' . $post_id . '-variation-' . $i
-		);
+	$my_post = array(
+		'post_title' => 'Variation #' . $i . ' of ' . esc_attr(strip_tags($_POST['productName'])) ,
+		'post_name' => 'product-' . $post_id . '-variation-' . $i,
+		'post_status' => 'publish',
+		'post_parent' => $post_id,
+		'post_type' => 'product_variation',
+		'guid' => home_url() . '/?product_variation=product-' . $post_id . '-variation-' . $i
+	);
 
 		// Insert the post into the database
 
-		wp_insert_post($my_post);
-		$variable_id = $post_id + 1;
-		$variable_two = $variable_id + 1;
-		$variable_three = $variable_two + 1;
-		$fullDimensions = $image_sizes['width'] . 'x' . $image_sizes['height'];
-		update_post_meta($variable_id, 'attribute_pa_resolution', 'large');
-		update_post_meta($variable_id, '_downloadable_files', $full);
-		update_post_meta($variable_id, '_downloadable', 'yes');
-		update_post_meta($variable_id, '_virtual', 'yes');
-		update_post_meta($variable_id, '_price', 220);
-		update_post_meta($variable_id, '_regular_price', '220');
-		update_post_meta($variable_id, '_credits_amount', '11');
-		update_post_meta($variable_id, '_stock_status', 'instock');
-		update_post_meta($variable_id, '_variation_description', $fullDimensions);
-		$mediumDimensions = $image_sizes['sizes']['download-medium']['width'] . 'x' . $image_sizes['sizes']['download-medium']['height'];
-		update_post_meta($variable_two, 'attribute_pa_resolution', 'medium');
-		update_post_meta($variable_two, '_downloadable_files', $medium);
-		update_post_meta($variable_two, '_downloadable', 'yes');
-		update_post_meta($variable_two, '_virtual', 'yes');
-		update_post_meta($variable_two, '_price', 180);
-		update_post_meta($variable_two, '_regular_price', '180');
-		update_post_meta($variable_two, '_credits_amount', '9');
-		update_post_meta($variable_two, '_stock_status', 'instock');
-		update_post_meta($variable_two, '_variation_description', $mediumDimensions);
-		$smallDimensions = $image_sizes['sizes']['download-small']['width'] . 'x' . $image_sizes['sizes']['download-small']['height'];
-		update_post_meta($variable_three, 'attribute_pa_resolution', 'small');
-		update_post_meta($variable_three, '_downloadable_files', $small);
-		update_post_meta($variable_three, '_downloadable', 'yes');
-		update_post_meta($variable_three, '_virtual', 'yes');
-		update_post_meta($variable_three, '_price', 160);
-		update_post_meta($variable_three, '_regular_price', '160');
-		update_post_meta($variable_three, '_credits_amount', '8');
-		update_post_meta($variable_three, '_stock_status', 'instock');
-		update_post_meta($variable_three, '_variation_description', $smallDimensions);
-		$i++;
-	}
+	$variable_id = wp_insert_post($my_post);
+	$fullDimensions = $image_sizes['width'] . 'x' . $image_sizes['height'];
+	update_post_meta($variable_id, 'attribute_pa_resolution', 'large');
+	update_post_meta($variable_id, '_downloadable', 'yes');
+	update_post_meta($variable_id, '_virtual', 'yes');
+	update_post_meta($variable_id, '_price', 220);
+	update_post_meta($variable_id, '_regular_price', '220');
+	update_post_meta($variable_id, '_credits_amount', 11);
+	update_post_meta($variable_id, '_stock_status', 'instock');
+	update_post_meta($variable_id, '_variation_description', $fullDimensions);
+	update_post_meta( $variable_id, '_download_permissions_granted', 1 );
+
+	$file_name = $sj_product_name . '-full';
+	sj_add_download_id($post_id, $variable_id, $sj_download_full[0], $file_name);
+
+	$variable_two = wp_insert_post($my_post);
+	$mediumDimensions = $image_sizes['sizes']['download-medium']['width'] . 'x' . $image_sizes['sizes']['download-medium']['height'];
+	update_post_meta($variable_two, 'attribute_pa_resolution', 'medium');
+	update_post_meta($variable_two, '_downloadable_files', $medium);
+	update_post_meta($variable_two, '_downloadable', 'yes');
+	update_post_meta($variable_two, '_virtual', 'yes');
+	update_post_meta($variable_two, '_price', 180);
+	update_post_meta($variable_two, '_regular_price', '180');
+	update_post_meta($variable_two, '_credits_amount', 9);
+	update_post_meta($variable_two, '_stock_status', 'instock');
+	update_post_meta($variable_two, '_variation_description', $mediumDimensions);
+	update_post_meta( $variable_two, '_download_permissions_granted', 1 );
+
+
+	$file_name = $sj_product_name . '-medium';
+	sj_add_download_id($post_id, $variable_two, $sj_download_medium[0], $file_name);
+
+	$variable_three = wp_insert_post($my_post);
+	$smallDimensions = $image_sizes['sizes']['download-small']['width'] . 'x' . $image_sizes['sizes']['download-small']['height'];
+	update_post_meta($variable_three, 'attribute_pa_resolution', 'small');
+	update_post_meta($variable_three, '_downloadable_files', $small);
+	update_post_meta($variable_three, '_downloadable', 'yes');
+	update_post_meta($variable_three, '_virtual', 'yes');
+	update_post_meta($variable_three, '_price', 160);
+	update_post_meta($variable_three, '_regular_price', '160');
+	update_post_meta($variable_three, '_credits_amount', 8);
+	update_post_meta($variable_three, '_stock_status', 'instock');
+	update_post_meta($variable_three, '_variation_description', $smallDimensions);
+	update_post_meta( $variable_three, '_download_permissions_granted', 1 );
+
+	$file_name = $sj_product_name . '-small';
+	sj_add_download_id($post_id, $variable_three, $sj_download_small[0], $file_name);
+
+	WC_Product_Variable::sync( $post_id, true );
+
+
 
 	$upload_model_release = isset($_POST['upload_model_release']) ? $_POST['upload_model_release'] : 'N/A';
 	$product = wc_get_product($post_id);
 	$product->update_meta_data('sj_model_release_form', $upload_model_release);
+	$product->update_meta_data('download_id', 123);
 	$product->save();
 	$upload_property_release = isset($_POST['upload_property_release']) ? $_POST['upload_property_release'] : 'N/A';
 	$product = wc_get_product($post_id);
@@ -261,35 +266,55 @@ function so_wp_ajax_function() {
 // Adding a custom tab
 add_action('woocommerce_product_write_panel_tabs', 'sj_license_tab');
 function sj_license_tab() {
-?>
+	?>
 	<li class="sj_license">
 		<a href="#sj_license_info">
 			<span><?php
 			_e('License', 'textdomain'); ?></span>
 		</a>
 	</li>
-<?php
+	<?php
 }
 
 add_action('woocommerce_product_data_panels', 'sj_license_tab_info');
 function sj_license_tab_info() {
-?>
+	?>
 	<div id="sj_license_info" class="panel woocommerce_options_panel">
-	<div class="options_group">
-	<?php
-	$field = array(
-		'id' => 'sj_model_release_form',
-		'label' => __('Model Release Form:', 'textdomain') ,
-	);
-	woocommerce_wp_text_input($field);
-	$field = array(
-		'id' => 'sj_property_release_form',
-		'label' => __('Property Release Form:', 'textdomain') ,
-	);
-	woocommerce_wp_text_input($field);
-?>
-	</div>
+		<div class="options_group">
+			<?php
+			$field = array(
+				'id' => 'sj_model_release_form',
+				'label' => __('Model Release Form:', 'textdomain') ,
+			);
+			woocommerce_wp_text_input($field);
+			$field = array(
+				'id' => 'sj_property_release_form',
+				'label' => __('Property Release Form:', 'textdomain') ,
+			);
+			woocommerce_wp_text_input($field);
+			?>
+		</div>
 	</div>
 
-<?php
+	<?php
+}
+
+
+function sj_add_download_id($post_id, $variable_id, $sj_img_url, $file_name){
+$sj_file = get_post_meta($post_id, '_downloadable_files'); // removed "true"
+
+        // NEW FILE: Setting the name, getting the url and and Md5 hash number
+$file_name = $file_name;
+$file_url  = $sj_img_url;
+$md5_num = md5( $file_url );
+
+        // Inserting new file in the exiting array of downloadable files
+$sj_file[0][$md5_num] = array(
+	'name'   =>  $file_name,
+	'file'   =>  $file_url
+);
+
+        // Updating database with the new array
+update_post_meta( $variable_id, '_downloadable_files', $sj_file[0] );
+
 }
